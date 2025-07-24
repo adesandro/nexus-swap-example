1126cb454ddae6b2a4da999120105c26b5164c2e
adesandro <74870324+adesandro@users.noreply.github.com>
2025-07-24 12:47:00 +0700

    Update TokenA.sol
    new name

--- contracts/TokenA.sol
+++ contracts/TokenA.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TokenA is ERC20 {
-    constructor() ERC20("Token A", "TKNA") {
+    constructor() ERC20("Token A", "TKNANEX") {
         _mint(msg.sender, 1_000_000 * 1e6); // 1M tokens, 6 decimals
     }
 
